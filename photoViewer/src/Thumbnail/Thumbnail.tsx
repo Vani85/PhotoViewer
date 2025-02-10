79be5973d6af384e95fbe200b4aa386a3f2bc890
Kalaivani Natarajan <kalaivani.natarajan@softwire.com>
2025-02-10 13:34:51 +0000

    Added props to parameterise the url

--- photoViewer/src/Thumbnail/Thumbnail.tsx
+++ photoViewer/src/Thumbnail/Thumbnail.tsx
@@ -1,6 +1,6 @@
 import './Thumbnail.css';
 
-export function DisplayThumbnail(props: { src: string | undefined; }) {   
+export function DisplayThumbnail(props: { src: string; }) {   
     return (  
             <img className = "thumbnailImage" src = {props.src}  alt="thumbnail image">
             </img> 
